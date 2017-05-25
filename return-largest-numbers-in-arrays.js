378786283b6c1fa208bdeb934b538ce736ec59c9
Diego <diegoviniciuscosta13@gmail.com>
2017-05-25 12:11:18 -0300

    Comitando desafios resolvidos até o momento

---
+++ return-largest-numbers-in-arrays.js
@@ -0,0 +1,5 @@
+//Return an array consisting of the largest number from each provided sub-array. For simplicity, the provided array will contain exactly 4 sub-arrays.
+function largestOfFour(arr) {
+  return arr.map(ar => ar.sort((a, b) => b - a)[0]);
+}
+console.log(largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]));
